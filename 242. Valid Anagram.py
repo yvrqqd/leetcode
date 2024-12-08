a6f240ba64e628beff8491b7cd178a2c23aea177
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2024-12-08 23:53:41 +0700

    Create 242. Valid Anagram.py

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        ds = defaultdict(int)
+        dt = defaultdict(int)
+
+        for i in range(len(s)):
+            ds[s[i]] += 1
+            dt[t[i]] += 1
+
+        return dt == ds
